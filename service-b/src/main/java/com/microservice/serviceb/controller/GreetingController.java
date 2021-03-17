959e0977ad1f1826a4ca40384925b779cb6e56d6
Birkan Atıcı <birkan.atici@sahibinden.com>
2021-03-18 01:05:30 +0300

    zipkin integration

--- service-b/src/main/java/com/microservice/serviceb/controller/GreetingController.java
+++ service-b/src/main/java/com/microservice/serviceb/controller/GreetingController.java
@@ -1,11 +1,16 @@
 package com.microservice.serviceb.controller;
 
+import static java.time.Instant.now;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @RestController
 @RequestMapping("/service-b")
 public class GreetingController
@@ -13,6 +18,8 @@ public class GreetingController
     @GetMapping("/hey")
     public ResponseEntity<String> hey()
     {
-        return new ResponseEntity<>("Hello from Service B.", HttpStatus.OK);
+        log.info("Request came.");
+
+        return new ResponseEntity<>(String.format("Hello from Service B. %n%nTime : %s", now()), HttpStatus.OK);
     }
 }
